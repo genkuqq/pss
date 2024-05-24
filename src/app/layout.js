d5f6598a2683fcc5b5662b9db3e5753173e1d152
genku <koraygench@gmail.com>
2024-05-24 03:35:41 +0300

    header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.scss";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html>
+      <Header />
       <body>{children}</body>
       <div className="background"></div>
     </html>
