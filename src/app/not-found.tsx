7c8af7b05276848f7854e57cb8efcc1e6557d352
genku <koraygench@gmail.com>
2024-07-03 04:01:03 +0300

    add not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import styles from "@/styles/notfound.module.scss";
+import styles from "@/styles/pagebase.module.scss";
 export default function NotFound() {
 	return (
-		<div className={styles.pagenotfound}>
+		<div className={styles.notfound}>
 			<FontAwesomeIcon className={styles.icon} icon={"triangle-exclamation"} />
 			<p>Böyle bir sayfa bulunmamakta – 404!</p>
 		</div>
